4472c009af6fcfef9179cfcefd5d14681f51b338
yupitz <alessandro.modolo.1988@gmail.com>
2014-01-08 09:50:26 +0100

    make some of the indexeddb's functions (continue, delete, ecc...) compliant with ES3. This resolves error messages during the production compile process

--- demo/app/lib/data/proxy/IndexedDB.js
+++ demo/app/lib/data/proxy/IndexedDB.js
@@ -1,12 +1,12 @@
 Ext.define('Lib.data.proxy.IndexedDB',{
-	alias:'proxy.indexeddb',
-	extend:'Ext.data.proxy.Client',
+    alias:'proxy.indexeddb',
+    extend:'Ext.data.proxy.Client',
     alternateClassName: 'Lib.IndexedDB',
 
     db:null,
 
     config:{
-    	/**
+        /**
          * @cfg {Object} reader
          * @hide
          */
@@ -31,19 +31,19 @@ Ext.define('Lib.data.proxy.IndexedDB',{
     },
 
     statics:{
-    	/*
-    	 * hash containing the table name and the associated model.
-		 * this is used during the db upgrade in order to create the table for all models that use this proxy
-    	 */
-    	registry:[]
+        /*
+         * hash containing the table name and the associated model.
+         * this is used during the db upgrade in order to create the table for all models that use this proxy
+         */
+        registry:[]
     },
 
     constructor: function(config){
-		this.callParent(arguments);
+        this.callParent(arguments);
         this.initConfig(config);
 
-		window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-	},
+        window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    },
 
     destroy: function() {
         this.db.close();
@@ -66,7 +66,7 @@ Ext.define('Lib.data.proxy.IndexedDB',{
 
             //register the model instance to the registry
             var registry = this.self.registry;
-        	registry[model.getProxy().getTable()]=model;
+            registry[model.getProxy().getTable()]=model;
         }
 
         this.callParent(arguments);
@@ -83,24 +83,24 @@ Ext.define('Lib.data.proxy.IndexedDB',{
 
         var request = window.indexedDB.open(me.getDatabase(), 1);
         request.onerror = function(event){
-        	if(typeof fail==='function')
+            if(typeof fail==='function')
                 fail.call(me, event.target.error);
         };
 
         request.onsuccess = function(event){
-			if(typeof success==='function')
-				success.call(me,event.target.result);
+            if(typeof success==='function')
+                success.call(me,event.target.result);
         };
         request.onupgradeneeded = function(event){
-        	var db = event.target.result,
+            var db = event.target.result,
                 registry = me.self.registry;
 
-        	for(var table in registry){
-        		if(!db.objectStoreNames.contains(table)){
-        			var key = registry[table].getIdProperty()||'id';
+            for(var table in registry){
+                if(!db.objectStoreNames.contains(table)){
+                    var key = registry[table].getIdProperty()||'id';
                     db.createObjectStore(table, { keyPath:key });
-        		}
-        	}
+                }
+            }
         };
     },
 
@@ -112,57 +112,57 @@ Ext.define('Lib.data.proxy.IndexedDB',{
      * @method
      */
     create: function(operation, callback, scope){
-		var me=this,
-    		records = operation.getRecords();
+        var me=this,
+            records = operation.getRecords();
 
-    	function openSuccess(db){
-    		operation.setStarted();
+        function openSuccess(db){
+            operation.setStarted();
 
-			var transaction=db.transaction(me.getTable(),'readwrite');
-			transaction.oncomplete = function(event){
-	            operation.setSuccessful();
-	            operation.setCompleted();
+            var transaction=db.transaction(me.getTable(),'readwrite');
+            transaction.oncomplete = function(event){
+                operation.setSuccessful();
+                operation.setCompleted();
 
-				if (typeof callback == 'function')
-		            callback.call(scope, operation);
-			};
-			transaction.onerror = function(event){
-	    		operation.setException(event.target.error);
+                if (typeof callback == 'function')
+                    callback.call(scope, operation);
+            };
+            transaction.onerror = function(event){
+                operation.setException(event.target.error);
 
-	    		if(typeof callback==='function')
-	    			callback.call(scope, operation);
-			};
+                if(typeof callback==='function')
+                    callback.call(scope, operation);
+            };
 
-			var objectStore= transaction.objectStore(me.getTable());
-			for (var i in records)
-            	addRecord(records[i]);
+            var objectStore= transaction.objectStore(me.getTable());
+            for (var i in records)
+                addRecord(records[i]);
 
             /**
              * Add a record to the database
              * @param {Object} record Record object to be inserted in the indexeddb
              * @method
              */
-			function addRecord(record){
-				objectStore.add(record.getData()).onsuccess = function(event){
-					//update record's info
-	                record.setId(event.target.result);
-	                record.phantom=false;
-	            }
-			};
-    	};
+            function addRecord(record){
+                objectStore.add(record.getData()).onsuccess = function(event){
+                    //update record's info
+                    record.setId(event.target.result);
+                    record.phantom=false;
+                }
+            };
+        };
 
-    	function openFail(error){
-    		operation.setException(error);
+        function openFail(error){
+            operation.setException(error);
 
-    		if(typeof callback==='function')
-    			callback.call(scope, operation);
-    	};
+            if(typeof callback==='function')
+                callback.call(scope, operation);
+        };
 
-    	this.open(openSuccess, openFail);
+        this.open(openSuccess, openFail);
     },
 
 
-	/**
+    /**
      * Performs the given read operation. 
      * @param {Ext.data.Operation} operation The Operation to perform
      * @param {Function} callback Callback function to be called when the Operation has completed (whether successful or not)
@@ -171,57 +171,57 @@ Ext.define('Lib.data.proxy.IndexedDB',{
      */
     read: function(operation, callback, scope){
         var me=this,
-    		records = operation.getRecords();
+            records = operation.getRecords();
 
-    	function openSuccess(db){
-    		var queryRecords=[];
+        function openSuccess(db){
+            var queryRecords=[];
 
-    		operation.setStarted();
+            operation.setStarted();
 
-	        var transaction=db.transaction(me.getTable());
-	        transaction.oncomplete = function(event){
-	        	operation.setRecords(queryRecords);
-	            operation.setResultSet(Ext.create('Ext.data.ResultSet', {
-	                records: operation.getRecords(),
-	                total  : queryRecords.length,
-	                count  : queryRecords.length
-	            }));
-	        	operation.setSuccessful();
-	            operation.setCompleted();
+            var transaction=db.transaction(me.getTable());
+            transaction.oncomplete = function(event){
+                operation.setRecords(queryRecords);
+                operation.setResultSet(Ext.create('Ext.data.ResultSet', {
+                    records: operation.getRecords(),
+                    total  : queryRecords.length,
+                    count  : queryRecords.length
+                }));
+                operation.setSuccessful();
+                operation.setCompleted();
 
-				if (typeof callback == 'function')
-		            callback.call(scope, operation);
-	        };
-	        transaction.onerror = function(event){
-	    		operation.setException(event.target.error);
+                if (typeof callback == 'function')
+                    callback.call(scope, operation);
+            };
+            transaction.onerror = function(event){
+                operation.setException(event.target.error);
 
-	    		if(typeof callback==='function')
-	    			callback.call(scope, operation);
-	        };
+                if(typeof callback==='function')
+                    callback.call(scope, operation);
+            };
 
 
-	        var objectStore= transaction.objectStore(me.getTable());
-	        objectStore.openCursor().onsuccess = function(event){
-	            var cursor = event.target.result;
+            var objectStore= transaction.objectStore(me.getTable());
+            objectStore.openCursor().onsuccess = function(event){
+                var cursor = event.target.result;
 
-	            if(cursor){
-	                var record = Ext.create(me.getModel(), cursor.value);
-	                record.phantom=false;
-	                queryRecords.push(record);
+                if(cursor){
+                    var record = Ext.create(me.getModel(), cursor.value);
+                    record.phantom=false;
+                    queryRecords.push(record);
 
-	                cursor.continue();
-	            }
-	        }
-    	};
+                    cursor['continue']();
+                }
+            }
+        };
 
-    	function openFail(error){
-    		operation.setException(error);
+        function openFail(error){
+            operation.setException(error);
 
-    		if(typeof callback==='function')
-    			callback.call(scope, operation);
-    	};
+            if(typeof callback==='function')
+                callback.call(scope, operation);
+        };
 
-    	this.open(openSuccess, openFail);
+        this.open(openSuccess, openFail);
     },
 
     /**
@@ -232,42 +232,42 @@ Ext.define('Lib.data.proxy.IndexedDB',{
      * @method
      */
     update: function(operation, callback, scope){
-    	var me=this,
-    		records = operation.getRecords();
+        var me=this,
+            records = operation.getRecords();
 
-    	function openSuccess(db){
-    		var me=this,
-	            records=operation.getRecords();
+        function openSuccess(db){
+            var me=this,
+                records=operation.getRecords();
 
-	        operation.setStarted();
-	        var transaction=db.transaction(me.getTable(),"readwrite");
-	        transaction.oncomplete = function(event){
-	            operation.setSuccessful();
-	            operation.setCompleted();
+            operation.setStarted();
+            var transaction=db.transaction(me.getTable(),"readwrite");
+            transaction.oncomplete = function(event){
+                operation.setSuccessful();
+                operation.setCompleted();
 
-	            if (typeof callback == 'function')
-	                callback.call(scope, operation);
-	        };
-	        transaction.onerror = function(event){
-	            operation.setException(event.target.error);
+                if (typeof callback == 'function')
+                    callback.call(scope, operation);
+            };
+            transaction.onerror = function(event){
+                operation.setException(event.target.error);
 
-	            if (typeof callback == 'function')
-	                callback.call(scope, operation);
-	        }
+                if (typeof callback == 'function')
+                    callback.call(scope, operation);
+            }
 
-	        var objectStore= transaction.objectStore(me.getTable());
-	         for (var i in records)
-	            objectStore.put(records[i].getData());
-    	};
+            var objectStore= transaction.objectStore(me.getTable());
+             for (var i in records)
+                objectStore.put(records[i].getData());
+        };
 
-    	function openFail(error){
-    		operation.setException(error);
+        function openFail(error){
+            operation.setException(error);
 
-    		if(typeof callback==='function')
-    			callback.call(scope, operation);
-    	};
+            if(typeof callback==='function')
+                callback.call(scope, operation);
+        };
 
-    	this.open(openSuccess, openFail);
+        this.open(openSuccess, openFail);
     },
 
     /**
@@ -278,62 +278,62 @@ Ext.define('Lib.data.proxy.IndexedDB',{
      * @method
      */
     destroy: function(operation, callback, scope){
-		var me=this,
-    		records = operation.getRecords();
+        var me=this,
+            records = operation.getRecords();
 
-    	function openSuccess(db){
-    		operation.setStarted();
+        function openSuccess(db){
+            operation.setStarted();
 
-	        var transaction=db.transaction(me.getTable(),"readwrite");
-	        transaction.oncomplete = function(event){
-	            operation.setSuccessful();
-	            operation.setCompleted();
+            var transaction=db.transaction(me.getTable(),"readwrite");
+            transaction.oncomplete = function(event){
+                operation.setSuccessful();
+                operation.setCompleted();
 
-	            if (typeof callback == 'function')
-	                callback.call(scope, operation);
-	        };
-	        transaction.onerror = function(event){
-	            operation.setException(event.target.error);
+                if (typeof callback == 'function')
+                    callback.call(scope, operation);
+            };
+            transaction.onerror = function(event){
+                operation.setException(event.target.error);
 
-	            if (typeof callback == 'function')
-	                callback.call(scope, operation);
-	        }
+                if (typeof callback == 'function')
+                    callback.call(scope, operation);
+            }
 
-	        var objectStore = transaction.objectStore(me.getTable());
-	        for (var i in records)
-	            objectStore.delete(records[i].getId());
-    	};
+            var objectStore = transaction.objectStore(me.getTable());
+            for (var i in records)
+                objectStore['delete'](records[i].getId());
+        };
 
-    	function openFail(error){
-    		operation.setException(error);
+        function openFail(error){
+            operation.setException(error);
 
-    		if(typeof callback==='function')
-    			callback.call(scope, operation);
-    	};
+            if(typeof callback==='function')
+                callback.call(scope, operation);
+        };
 
-    	this.open(openSuccess, openFail);
+        this.open(openSuccess, openFail);
     },
 
     clear: function(){
-    	var me=this;
+        var me=this;
 
-    	function openSuccess(db){
-    		var objectStore = db.transaction(me.getTable(),"readwrite")
-	                            .objectStore(me.getTable());
+        function openSuccess(db){
+            var objectStore = db.transaction(me.getTable(),"readwrite")
+                                .objectStore(me.getTable());
 
-	        objectStore.openCursor().onsuccess = function(event){
-	            var cursor = event.target.result;
-	            if(cursor){
-	                objectStore.delete(cursor.key);
-	                cursor.continue();
-	            }
-	        }
-    	};
+            objectStore.openCursor().onsuccess = function(event){
+                var cursor = event.target.result;
+                if(cursor){
+                    objectStore['delete'](cursor.key);
+                    cursor['continue']();
+                }
+            }
+        };
 
-    	function openFail(error){
-    		//log the error
-    	};
+        function openFail(error){
+            //log the error
+        };
 
-    	this.open(openSuccess, openFail);
+        this.open(openSuccess, openFail);
     }
 });
